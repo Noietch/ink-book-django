c4410fcf76906904f0c096b019dbc94e50375765
19241078 <19241078@buaa.edu.cn>
2022-08-01 18:23:15 +0800

    projects views

--- ink_book_django/projects/urls.py
+++ ink_book_django/projects/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-
+    url(r'^projects/$', ProjectListAPIView.as_view()),
+    url(r'^projects/(?P<pk>\d+)/$', ProjectDetailAPIView.as_view()),
 ]
 
-router = DefaultRouter()
-router.register(r'projects', ProjectModelViewSet)
-urlpatterns += router.urls
+# router = DefaultRouter()
+# router.register(r'projects', ProjectModelViewSet)
+# urlpatterns += router.urls
