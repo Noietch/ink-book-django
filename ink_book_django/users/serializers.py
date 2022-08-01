46d202911123f6fbb727807d89e3617f71fc73ef
19241078 <19241078@buaa.edu.cn>
2022-08-01 20:48:07 +0800

    projects views

--- ink_book_django/users/serializers.py
+++ ink_book_django/users/serializers.py
@@ -5,4 +5,4 @@ from .models import Users
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
-        fields = ('id','email', 'username', 'avatar', 'real_name')
+        fields = ('id', 'email', 'username', 'avatar', 'real_name')
