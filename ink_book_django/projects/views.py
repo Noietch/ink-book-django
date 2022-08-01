0810d15babd7121cf8dacd133399284412385c3d
19241078 <19241078@buaa.edu.cn>
2022-08-01 21:07:46 +0800

    project view finish :tada:

--- ink_book_django/projects/views.py
+++ ink_book_django/projects/views.py
@@ -22,13 +22,19 @@ class ListAPIView(APIView):
 
     def post(self, request):
         serializer = self.serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        res = {
-            'code': 1001,
-            'msg': '添加成功',
-            'data': serializer.validated_data
-        }
+        if serializer.is_valid():
+            serializer.save()
+            res = {
+                'code': 1001,
+                'msg': '添加成功',
+                'data': serializer.data
+            }
+        else:
+            res = {
+                'code': 1002,
+                'msg': '添加失败',
+                'data': serializer.data
+            }
         return Response(res)
 
 
@@ -36,17 +42,23 @@ class DetailAPIView(APIView):
     model = None
     serializer = None
 
-    def get(self, request, pk):
+    def get_object(self, pk):
         try:
-            object = self.model.objects.get(id=pk)
+            obj = self.model.objects.get(id=pk)
         except self.model.DoesNotExist:
+            return None
+        return obj
+
+    def get(self, request, pk):
+        obj = self.get_object(pk)
+        if obj is None:
             return Response({
                 'code': 1002,
                 'msg': '对象不存在',
                 'data': None
-                })
+            })
 
-        serializer = self.serializer(instance=object)
+        serializer = self.serializer(obj)
         res = {
             'code': 1001,
             'msg': '查询成功',
@@ -55,36 +67,65 @@ class DetailAPIView(APIView):
         return Response(res)
 
     def put(self, request, pk):
-        try:
-            object = self.model.objects.get(id=pk)
-        except self.model.DoesNotExist:
+        obj = self.get_object(pk)
+        if obj is None:
             return Response({
                 'code': 1002,
                 'msg': '对象不存在',
                 'data': None
             })
 
-        serializer = self.serializer(instance=object, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        res = {
-            'code': 1001,
-            'msg': '修改成功',
-            'data': serializer.data
-        }
+        serializer = self.serializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            res = {
+                'code': 1001,
+                'msg': '修改成功',
+                'data': serializer.data
+            }
+        else:
+            res = {
+                'code': 1003,
+                'msg': '修改失败',
+                'data': serializer.data
+            }
+        return Response(res)
+
+    def patch(self, request, pk):
+        obj = self.get_object(pk)
+        if obj is None:
+            return Response({
+                'code': 1002,
+                'msg': '对象不存在',
+                'data': None
+            })
+
+        serializer = self.serializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {
+                'code': 1001,
+                'msg': '修改成功',
+                'data': serializer.data
+            }
+        else:
+            res = {
+                'code': 1003,
+                'msg': '修改失败',
+                'data': serializer.data
+            }
         return Response(res)
 
     def delete(self, request, pk):
-        try:
-            object = self.model.objects.get(id=pk)
-        except self.model.DoesNotExist:
+        obj = self.get_object(pk)
+        if obj is None:
             return Response({
                 'code': 1002,
                 'msg': '对象不存在',
                 'data': None
             })
 
-        object.delete()
+        obj.delete()
         res = {
             'code': 1001,
             'msg': '删除成功',
@@ -101,25 +142,6 @@ class ProjectListAPIView(ListAPIView):
 class ProjectDetailAPIView(DetailAPIView):
     model = Project
     serializer = ProjectModelSerializer
-
-    def patch(self, request, pk):
-        try:
-            project = Project.objects.get(id=pk)
-        except Project.DoesNotExist:
-            return Response({
-                'code': 1002,
-                'msg': '对象不存在',
-                'data': None
-            })
-
-        project.name = request.data['name']
-        serializer = ProjectModelSerializer(instance=project)
-        res = {
-            'code': 1001,
-            'msg': '修改成功',
-            'data': serializer.data
-        }
-        return Response(res)
 
 
 class PrototypeListAPIView(ListAPIView):
