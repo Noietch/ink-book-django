33003ca13353bbb085f7168a3ebf5370ffb9a4b8
19241078 <19241078@buaa.edu.cn>
2022-08-03 12:47:51 +0800

    add post API :tada:

--- ink_book_django/projects/urls.py
+++ ink_book_django/projects/urls.py
@@ -1,17 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from django.urls import re_path
+from django.urls import path
 
 from .views import *
 
 urlpatterns = [
-    re_path(r'^projects/$', ProjectListAPIView.as_view()),
-    re_path(r'^projects/(?P<pk>\d+)/$', ProjectDetailAPIView.as_view()),
-    re_path(r'^prototypes/$', PrototypeListAPIView.as_view()),
-    re_path(r'^prototypes/(?P<pk>\d+)/$', PrototypeDetailAPIView.as_view()),
-    re_path(r'^umls/$', UMLListAPIView.as_view()),
-    re_path(r'^umls/(?P<pk>\d+)/$', UMLDetailAPIView.as_view()),
-    re_path(r'^documents/$', DocumentListAPIView.as_view()),
-    re_path(r'^documents/(?P<pk>\d+)/$', DocumentDetailAPIView.as_view()),
+    path(r'projects/', ProjectListAPIView.as_view()),
+    path(r'projects/<int:pk>/', ProjectDetailAPIView.as_view()),
+    path(r'prototypes/', PrototypeListAPIView.as_view()),
+    path(r'prototypes/<int:pk>/', PrototypeDetailAPIView.as_view()),
+    path(r'umls/', UMLListAPIView.as_view()),
+    path(r'umls/<int:pk>/', UMLDetailAPIView.as_view()),
+    path(r'documents/', DocumentListAPIView.as_view()),
+    path(r'documents/<int:pk>/', DocumentDetailAPIView.as_view()),
 ]
 
 # router = DefaultRouter()
