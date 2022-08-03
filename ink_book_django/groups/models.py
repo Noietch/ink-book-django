d04e0c10e3edc4f7f31d68fd7dce12274f59934a
19241078 <19241078@buaa.edu.cn>
2022-08-03 17:42:03 +0800

    add random avatar :rocket:

--- ink_book_django/groups/models.py
+++ ink_book_django/groups/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from random import randint
 
 
 class Groups(models.Model):
-    name = models.CharField(max_length=10)
+    AVATARS = [
+        'https://dn-st.teambition.net/teambition/images/logo1.a6464e9c.jpg',
+        'https://dn-st.teambition.net/teambition/images/logo3.c69aba1a.jpg',
+        'https://dn-st.teambition.net/teambition/images/logo5.a07a0ef0.jpg',
+        'https://img.alicdn.com/imgextra/i3/O1CN01fYx7ZL1F2WfrXat17_!!6000000000429-2-tps-600-264.png'
+    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.avatar is None:
+            self.avatar = self.AVATARS[randint(0, 3)]
+
+    name = models.CharField(max_length=50)
     creator = models.IntegerField()
     avatar = models.TextField(null=True)
 
