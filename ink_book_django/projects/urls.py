a6be8c7145d665fc945d20fa7093dff16dda8d1c
19241078 <19241078@buaa.edu.cn>
2022-08-07 16:31:59 +0800

    add document center api

--- ink_book_django/projects/urls.py
+++ ink_book_django/projects/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path(r'projects/<int:pk>/', ProjectDetailAPIView.as_view()),
     path(r'prototypes/', PrototypeListAPIView.as_view()),
     path(r'prototypes/info/', PrototypeInfoAPIView.as_view()),
+    path(r'prototypes/users/', PrototypeUserAPIView.as_view()),
     path(r'prototypes/<int:pk>/', PrototypeDetailAPIView.as_view()),
     path(r'umls/', UMLListAPIView.as_view()),
     path(r'umls/<int:pk>/', UMLDetailAPIView.as_view()),
